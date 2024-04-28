870c6158f115b02d563dcf860dd15b15f2bac97a
devEdylan <edybonfim2005@gmail.com>
2024-04-28 11:37:15 -0300

    Escrita dos tipos de variáveis (descontando String)

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte menor = -128 + 127;
+        short pequeno = -32768 + 32768;
+        int medio = -2147483648 + 2147483647;
+        long longo = -9223372036854770000L + 9223372036854770000L;
+        boolean certo = true;
+        char caractere = 'u';
+        float quebrado = -0.0000014024F + 0.0000014024F;
+        double quebrado_o_dobro = -0.000000494 + 0.000000494;
     }
 }
